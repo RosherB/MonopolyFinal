475856e7b39b56e379bff6a825370880631d9a3e
ehu <ehu@U105289>
2023-09-29 10:08:00 +0200

    a

--- src/main/java/edu/ncsu/monopoly/Card.java
+++ src/main/java/edu/ncsu/monopoly/Card.java
@@ -1,6 +1,5 @@
 package edu.ncsu.monopoly;
 //updated 20222 with a comment
-//I	have	added	a	comment
 public abstract class Card {
 
     public static final int TYPE_CHANCE = 1;
@@ -10,3 +9,4 @@ public abstract class Card {
     public abstract void applyAction();
     public abstract int getCardType();
 }
+//a
